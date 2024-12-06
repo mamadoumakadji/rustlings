d3c42cf38d16b4c2572f4e51742c49a0fa55e509
mamadoumakadji <makadjimamadou01@gmail.com>
2024-12-06 21:38:32 +0100

    add if

---
+++ if/if2.rs
@@ -0,0 +1,42 @@
+// TODO: Fix the compiler error on this function.
+fn picky_eater(food: &str) -> &str {
+    if food == "strawberry" {
+        "Yummy!"
+    } else if food == "potato" {
+        "I guess I can eat that."
+    } else {
+        "No thanks!"
+    }
+}
+
+fn main() {
+    // Vous pouvez expérimenter ici si besoin.
+    println!("{}", picky_eater("strawberry")); // Affiche "Yummy!"
+    println!("{}", picky_eater("potato")); // Affiche "I guess I can eat
+     
+}
+
+// TODO: Read the tests to understand the desired behavior.
+// Make all tests pass without changing them.
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn yummy_food() {
+        // Cela signifie que l'appel de `picky_eater` avec l'argument "food" doit retourner "Yummy!".
+        assert_eq!(picky_eater("strawberry"), "Yummy!");
+    }
+
+    #[test]
+    fn neutral_food() {
+        assert_eq!(picky_eater("potato"), "I guess I can eat that.");
+    }
+
+    #[test]
+    fn default_disliked_food() {
+        assert_eq!(picky_eater("broccoli"), "No thanks!");
+        assert_eq!(picky_eater("gummy bears"), "No thanks!");
+        assert_eq!(picky_eater("literally anything"), "No thanks!");
+    }
+}
